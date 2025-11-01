c124d5668ed840868785ade9dded145d0d7c8594
SanzGitHere <shanjeyangithub@gmail.com>
2025-11-01 12:50:02 +0530

    Update route.js

--- config/inngest.js
+++ config/inngest.js
@@ -1,4 +1,6 @@
 import { Inngest } from "inngest";
+import connectDB from "./db";
+import User from "@/models/User";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
@@ -12,6 +14,50 @@ export const syncUserCreation = inngest.createFunction(
         event : 'clerk/user.created'
     },
     async ({event})=>{
+        const {id, first_name, last_name, email_addresses, image_url} = event.data
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_addresses,
+            name: first_name + ' ' + last_name,
+            image_Url: image_url 
+        }
+        await connectDB()
+        await User.create(userData)    
+    }
+)
 
+// Inngest Function to update user data in the database
+export const syncUserUpdation = inngest.createFunction(
+    {
+        id: 'update-user-from-clerk'
+    },
+    {
+        event: 'clerk/user.updated'
+    },
+    async ({event})=>{
+        const {id, first_name, last_name, email_addresses, image_url} = event.data
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_addresses,
+            name: first_name + ' ' + last_name,
+            image_Url: image_url 
+        }
+        await connectDB()
+        await User.findByIdAndUpdate(id, userData)
+    }    
+)
+
+// Inngest Function to delete user from the database
+export const syncUserDeletion = inngest.createFunction(
+    {
+        id: 'delete-user-with-clerk'
+    },
+    {
+        event: 'clerk/user.deleted'
+    },
+    async ({event})=>{
+        const {id} = event.data
+        await connectDB()
+        await User.findByIdAndDelete(id)
     }
 )
